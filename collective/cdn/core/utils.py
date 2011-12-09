40143adfa2b36be0527d5ab133d0d29de2096ef5
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-09 00:52:07 -0200

    PEP 8 cleanup

--- collective/cdn/core/utils.py
+++ collective/cdn/core/utils.py
@@ -6,23 +6,28 @@ from Products.CMFCore.interfaces import IPropertiesTool
 
 
 def cdn_config():
+    ''' Return CDN Configuration '''
     ptool = queryUtility(IPropertiesTool)
     if ptool is None:
         return None
     props = getattr(ptool, 'cdn_properties', None)
     return props
 
+
 def image_cdn_enabled():
+    ''' Is Image CDN also available? '''
     config = cdn_config()
-    return config and config.getProperty('enable_cdn_image',False)
+    return config and config.getProperty('enable_cdn_image', False)
+
 
 def getProvider():
+    ''' Return the activated provider '''
     config = cdn_config()
-    provider_name = config.getProperty('cdn_provider','CoralCDN')
-    p_hostname = config.getProperty('cdn_hostname','')
-    p_port = config.getProperty('cdn_port',80)
-    p_path = config.getProperty('cdn_path','')
-    provider = getUtility(ICDNProvider,provider_name)(hostname=p_hostname,
-                                                      port=p_port,
-                                                      path=p_path)
+    provider_name = config.getProperty('cdn_provider', 'CoralCDN')
+    p_hostname = config.getProperty('cdn_hostname', '')
+    p_port = config.getProperty('cdn_port', 80)
+    p_path = config.getProperty('cdn_path', '')
+    provider = getUtility(ICDNProvider, provider_name)(hostname=p_hostname,
+                                                       port=p_port,
+                                                       path=p_path)
     return provider
