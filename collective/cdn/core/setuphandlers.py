89451b2c1354b2094fde349a6de52adb71cd1259
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-09 01:01:51 -0200

    Major cleanup, also PEP8

--- collective/cdn/core/setuphandlers.py
+++ collective/cdn/core/setuphandlers.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
-from zope import component
 import logging
 from Products.CMFCore.utils import getToolByName
-from Products.GenericSetup import interfaces as gsinterfaces
+
 from Products.GenericSetup.upgrade import listUpgradeSteps
 
-from Products.ZCatalog.ProgressHandler import ZLogHandler
-
-try:
-    from Products.CacheSetup import interfaces
-    from Products.CacheSetup.enabler import enableCacheFu
-    CACHEFU = True
-except ImportError:
-    CACHEFU = False
 
 _PROJECT = 'collective.cdn.core'
 _PROFILE_ID = 'collective.cdn.core:default'
@@ -23,19 +14,22 @@ def doUpgrades(context):
     '''
     if context.readDataFile('collective.cdn.core.txt') is None:
         return
+
     logger = logging.getLogger(_PROJECT)
     site = context.getSite()
-    setup_tool = getToolByName(site,'portal_setup')
-    cache = CACHEFU and getToolByName(context,'portal_cache_settings',None)
+    setup_tool = getToolByName(site, 'portal_setup')
+    cache = getToolByName(context, 'portal_cache_settings', None)
+    enable_cache = False
     version = setup_tool.getLastVersionForProfile(_PROFILE_ID)
-    upgradeSteps = listUpgradeSteps(setup_tool,_PROFILE_ID, version)
-    sorted(upgradeSteps,key=lambda step:step['sortkey'])
-    
-    if cache:
-        # Desabilitamos o cache fu para nao termos uma enxurrada
-        # de purges
+    upgradeSteps = listUpgradeSteps(setup_tool, _PROFILE_ID, version)
+    sorted(upgradeSteps, key=lambda step: step['sortkey'])
+
+    if cache and cache.getEnabled():
+        # In case we have a cache fu, disable it to avoid a
+        # tsunami of purges
         cache.setEnabled(False)
-        
+        enable_cache = True
+
     for step in upgradeSteps:
         oStep = step.get('step')
         if oStep is not None:
@@ -44,8 +38,7 @@ def doUpgrades(context):
                                                           _PROFILE_ID)
             setup_tool.setLastVersionForProfile(_PROFILE_ID, oStep.dest)
             logger.info(msg)
-    
-    if cache:
-        # Novamente habilitamos o cache fu para nao termos uma enxurrada
-        # de purges
+
+    if cache and enable_cache:
+        # Now, turn cachefu back to normal
         cache.setEnabled(True)
