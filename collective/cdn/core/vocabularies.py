a7f4802d3a8a8b2fa89461261fbbadfb87dcdf54
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-09 00:49:41 -0200

    PEP 8 cleanup

--- collective/cdn/core/vocabularies.py
+++ collective/cdn/core/vocabularies.py
@@ -1,23 +1,31 @@
 # -*- coding: utf-8 -*-
-from zope.schema.interfaces import IVocabularyFactory
-from Products.CMFCore.utils import getToolByName
+
+from zope.interface import implements
+from zope.component import getUtilitiesFor
+
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-from zope.component import getMultiAdapter, getUtilitiesFor, getUtility
-from zope.interface import implements
+
+from zope.schema.interfaces import IVocabularyFactory
+
 from collective.cdn.core.interfaces import ICDNProvider
+
 
 class ProvidersVocabulary(object):
     """ Return Available Providers
     """
+
     implements(IVocabularyFactory)
-    
+
     def __call__(self, context):
-        ''' 
+        ''' List available providers registered for ICDNProvider
         '''
         utilities = getUtilitiesFor(ICDNProvider)
-        
-        vocab = [SimpleTerm(name, name) for (name,instance) in utilities if name.strip()]
+
+        vocab = [SimpleTerm(name, name) for (name, instance) in utilities
+                                                             if name.strip()]
+
         return SimpleVocabulary(vocab)
+
 
 ProvidersVocabularyFactory = ProvidersVocabulary()
