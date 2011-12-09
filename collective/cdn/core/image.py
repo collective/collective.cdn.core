80ef39bfa8a1d6887a78481f46b3342980ee7368
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-09 01:06:08 -0200

    PEP 8 cleanup

--- collective/cdn/core/image.py
+++ collective/cdn/core/image.py
@@ -1,10 +1,15 @@
 # -*- coding:utf-8 -*-
 from cgi import escape
+
 from zope.app.component.hooks import getSite
+
 from Products.Archetypes.Field import ImageField
-from collective.cdn.core.utils import image_cdn_enabled, getProvider
-        
-def base_url(self,instance):
+
+from collective.cdn.core.utils import getProvider
+from collective.cdn.core.utils import image_cdn_enabled
+
+
+def base_url(self, instance):
     url = instance.absolute_url()
     if not image_cdn_enabled():
         return url
@@ -12,8 +17,9 @@ def base_url(self,instance):
     base_url = site_url
     path = url[len(site_url)+1:]
     provider = getProvider()
-    return '%s/%s' % (provider.process_url(base_url,path),path)
-    
+    return '%s/%s' % (provider.process_url(base_url, path), path)
+
+
 def tag(self, instance, scale=None, height=None, width=None, alt=None,
         css_class=None, title=None, **kwargs):
     """Create a tag including scale
@@ -41,11 +47,11 @@ def tag(self, instance, scale=None, height=None, width=None, alt=None,
     if title is None:
         title = instance.Title()
 
-    values = {'src' : url,
-              'alt' : escape(alt, quote=True),
-              'title' : escape(title, quote=True),
-              'height' : height,
-              'width' : width,
+    values = {'src': url,
+              'alt': escape(alt, quote=True),
+              'title': escape(title, quote=True),
+              'height': height,
+              'width': width,
              }
 
     result = '<img src="%(src)s" alt="%(alt)s" title="%(title)s" '\
@@ -59,6 +65,7 @@ def tag(self, instance, scale=None, height=None, width=None, alt=None,
             result = '%s %s="%s"' % (result, key, value)
 
     return '%s />' % result
+
 
 def patch():
     ''' Patch ImageField with our tag method
