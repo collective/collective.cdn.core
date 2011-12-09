7547e244a8f37370216961ab975a621a4c381d91
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-09 00:53:34 -0200

    PEP 8 cleanup

--- collective/cdn/core/__init__.py
+++ collective/cdn/core/__init__.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-
 from zope.i18nmessageid import MessageFactory as BaseMessageFactory
-
-from collective.cdn.core import config
-
-from Products.Archetypes import atapi
-from Products.CMFCore import utils
 
 from collective.cdn.core import image
 
