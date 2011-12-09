a51b0e04d1282683c6dcd048ed3e680286797251
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-09 00:55:27 -0200

    PEP 8 cleanup

--- collective/cdn/core/interfaces/provider.py
+++ collective/cdn/core/interfaces/provider.py
@@ -1,10 +1,11 @@
 # -*- coding=utf-8 -*-
 from zope.interface import Interface
 
+
 class ICDNProvider(Interface):
     '''A CDN Provider
     '''
-    
+
     def process_url(url, relative_path=''):
-        ''' process a url
+        ''' Process a url
         '''
