ba8d22569979766e855a5513bc5c5794dcf8fcd1
Érico Andrei <erico@simplesconsultoria.com.br>
2012-08-01 12:40:25 -0300

    Add plone.app.testing

--- setup.py
+++ setup.py
@@ -36,6 +36,9 @@ setup(name='collective.cdn.core',
           'setuptools',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+        'test': ['plone.app.testing'],
+      },
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
